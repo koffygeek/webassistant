aebfe852d316db174c2876c868eee52c3cf0e223
Yvan Grebe <yvan.grebe@gmail.com>
2025-05-14 20:11:04 +0100

    debut

---
+++ auth.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/prisma"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+
+import NextAuth from "next-auth"
+ 
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+})
