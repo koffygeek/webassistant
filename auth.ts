471ee45c9f943715560f9d9d0a1c97bd2e851efc
Yvan Grebe <yvan.grebe@gmail.com>
2025-05-15 01:44:04 +0100

    error migration 5432

--- auth.ts
+++ auth.ts
@@ -1,9 +1,9 @@
-import { prisma } from "@/prisma"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-import NextAuth from "next-auth"
- 
+import NextAuth from "next-auth";
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [],
-})
+});
